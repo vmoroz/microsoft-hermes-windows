2b63baf17ea0db343a68935240ed9eb83c4dc297
Vladimir Morozov <vmorozov@microsoft.com>
2025-03-05 13:25:33 -0800

    Add comments to child_process

--- unittests/NodeAPI/child_process.h
+++ unittests/NodeAPI/child_process.h
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#pragma once
-#ifndef CHILD_PROCESS_H_
-#define CHILD_PROCESS_H_
+#ifndef HERMES_UNITTESTS_NODEAPI_CHILD_PROCESS_H
+#define HERMES_UNITTESTS_NODEAPI_CHILD_PROCESS_H
 
 #include <string>
 #include <string_view>
@@ -11,15 +10,18 @@
 
 namespace node_api_tests {
 
+// Struct to hold the result of a child process execution.
 struct ProcessResult {
-  uint32_t status;
-  std::string std_output;
-  std::string std_error;
+  uint32_t status; // Exit status of the child process.
+  std::string std_output; // Standard output from the child process.
+  std::string std_error; // Standard error from the child process.
 };
 
+// Creates a child process to run the given command with the specified
+// arguments.
 ProcessResult spawnSync(std::string_view command,
                         std::vector<std::string> args);
 
 }  // namespace node_api_tests
 
-#endif  // !CHILD_PROCESS_H_
+#endif  // !HERMES_UNITTESTS_NODEAPI_CHILD_PROCESS_H
