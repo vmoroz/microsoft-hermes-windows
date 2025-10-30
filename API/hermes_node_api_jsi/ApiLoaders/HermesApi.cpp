bc11c8cd2f1904ef6ac7bf72dca5a6ede0f3140c
Vladimir Morozov <vmorozov@microsoft.com>
2025-10-30 12:07:31 -0700

    Format code

--- API/hermes_node_api_jsi/ApiLoaders/HermesApi.cpp
+++ API/hermes_node_api_jsi/ApiLoaders/HermesApi.cpp
@@ -5,10 +5,13 @@
 
 // Define cross-platform pragma macros for suppressing offsetof warnings
 #ifdef __clang__
-#define HERMES_PRAGMA_PUSH_OFFSETOF _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Winvalid-offsetof\"")
+#define HERMES_PRAGMA_PUSH_OFFSETOF \
+  _Pragma("GCC diagnostic push")    \
+      _Pragma("GCC diagnostic ignored \"-Winvalid-offsetof\"")
 #define HERMES_PRAGMA_POP_OFFSETOF _Pragma("GCC diagnostic pop")
 #else
-#define HERMES_PRAGMA_PUSH_OFFSETOF __pragma(warning(push)) __pragma(warning(disable : 4117))
+#define HERMES_PRAGMA_PUSH_OFFSETOF \
+  __pragma(warning(push)) __pragma(warning(disable : 4117))
 #define HERMES_PRAGMA_POP_OFFSETOF __pragma(warning(pop))
 #endif
 
@@ -22,14 +25,17 @@ struct HermesApiNames {
 #include "HermesApi.inc"
 };
 
-// Load all inspector functions together to ensure their availability from different threads.
+// Load all inspector functions together to ensure their availability from
+// different threads.
 void loadInspectorFuncs() {
   HermesApi *current = HermesApi::current();
-#define HERMES_INSPECTOR_FUNC(func)                                                                                  \
-  HERMES_PRAGMA_PUSH_OFFSETOF                                                                                        \
-  decltype(::func) *loaded_##func = reinterpret_cast<decltype(::func) *>(current->getFuncPtr(HermesApiNames::func)); \
-  size_t offset_##func = offsetof(HermesApi, func);                                                                  \
-  *reinterpret_cast<decltype(::func) **>(reinterpret_cast<char *>(current) + offset_##func) = loaded_##func;        \
+#define HERMES_INSPECTOR_FUNC(func)                                       \
+  HERMES_PRAGMA_PUSH_OFFSETOF                                             \
+  decltype(::func) *loaded_##func = reinterpret_cast<decltype(::func) *>( \
+      current->getFuncPtr(HermesApiNames::func));                         \
+  size_t offset_##func = offsetof(HermesApi, func);                       \
+  *reinterpret_cast<decltype(::func) **>(                                 \
+      reinterpret_cast<char *>(current) + offset_##func) = loaded_##func; \
   HERMES_PRAGMA_POP_OFFSETOF
 #include "HermesApi.inc"
 }
@@ -40,16 +46,23 @@ thread_local HermesApi *HermesApi::current_{};
 
 HermesApi::HermesApi(IFuncResolver *funcResolver)
     : JSRuntimeApi(funcResolver)
-#define HERMES_FUNC(func)                                                                                       \
-  HERMES_PRAGMA_PUSH_OFFSETOF                                                                                   \
-  , func(&ApiFuncResolver<HermesApi, decltype(::func) *, HermesApiNames::func, offsetof(HermesApi, func)>::stub) \
-  HERMES_PRAGMA_POP_OFFSETOF
-#define HERMES_INSPECTOR_FUNC(func)                                                                           \
-  HERMES_PRAGMA_PUSH_OFFSETOF                                                                                 \
-  ,                                                                                                           \
-      func(&ApiFuncResolver<HermesApi, decltype(::func) *, HermesApiNames::func, offsetof(HermesApi, func)>:: \
-               preloadStub<&loadInspectorFuncs>)                                                              \
-  HERMES_PRAGMA_POP_OFFSETOF
+#define HERMES_FUNC(func)        \
+  HERMES_PRAGMA_PUSH_OFFSETOF    \
+  ,                              \
+      func(&ApiFuncResolver<     \
+           HermesApi,            \
+           decltype(::func) *,   \
+           HermesApiNames::func, \
+           offsetof(HermesApi, func)>::stub) HERMES_PRAGMA_POP_OFFSETOF
+#define HERMES_INSPECTOR_FUNC(func)                                      \
+  HERMES_PRAGMA_PUSH_OFFSETOF                                            \
+  ,                                                                      \
+      func(&ApiFuncResolver<                                             \
+           HermesApi,                                                    \
+           decltype(::func) *,                                           \
+           HermesApiNames::func,                                         \
+           offsetof(HermesApi, func)>::preloadStub<&loadInspectorFuncs>) \
+          HERMES_PRAGMA_POP_OFFSETOF
 
 #include "HermesApi.inc"
 {
